c90c0da477ae0b07789e2f22cba87a8524055175
Mokarama <mokaramaaktershanta@gmail.com>
2025-06-19 19:07:50 +0600

    create basic header with logo and tagline

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hader from '../components/Hader';
+
+const HomeLayout = () => {
+    return (
+       <div>
+         <hader>
+            <Hader></Hader>
+         </hader>
+         <nav></nav>
+         <main></main>
+       </div>
+    );
+};
+
+export default HomeLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { Router, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import router from './routes/router.jsx'
 
 createRoot(document.getElementById('root')).render(
